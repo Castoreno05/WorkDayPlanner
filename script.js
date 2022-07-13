3f18090808a117051d703835add1b3f60dea9ce6
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-13 08:58:26 -0500

    Images added. Save buttons for the time block were added. Gave them the ability to save the text in the input of the blocks.

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
+// Need day of the week and date displayed at the top of the page
 var dayMonth = moment().format("dddd, MMMM Do");
 $("#currentDay").text(dayMonth);
 
 
-var saveButton = document.getElementById("saveIcon");
-var saveButtonTwo = document.getElementById("saveIcon2");
-var saveButtonThree = document.getElementById("saveIcon3");
-var saveButtonFour = document.getElementById("saveIcon4");
-var saveButtonFive = document.getElementById("saveIcon5");
-var saveButtonSix = document.getElementById("saveIcon6");
-var saveButtonSeven = document.getElementById("saveIcon7");
-var saveButtonEight = document.getElementById("saveIcon8");
-var saveButtonNine = document.getElementById("saveIcon9");
+// Variable for the save button
+var saveButton = $("div.saveBtn")
 
-saveButton.addEventListener("click", save);
-saveButtonTwo.addEventListener("click", save);
-saveButtonThree.addEventListener("click", save);
-saveButtonFour.addEventListener("click", save);
-saveButtonFive.addEventListener("click", save);
-saveButtonSix.addEventListener("click", save);
-saveButtonSeven.addEventListener("click", save);
-saveButtonEight.addEventListener("click", save);
-saveButtonNine.addEventListener("click", save);
+// onclick for save button. When clicked the text in the input feild should save
+saveButton.on("click", function () {
+    // On this click we want to look for the text that the user has entered.
+    var input = $(this).siblings(".time-block").children(".form-input")
+    var text = input.val()
+    // Then save it to local storage
+    var time = $(this).siblings(".time").attr("id")
+    localStorage.setItem(time, text);
+});
 
-function save() {
-    console.log("you have clicked the icon"); 
+
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+}
+
+
+// We need to get the value. 
+// loops
+for (let index = 9; index <= 17 ; index++) {
+    document.getElementById("event" + index).value = localStorage.getItem("time"+ index)
+}
+
+
+// current hours
+var date = new Date();
+var currentHour = date.getHours()
+
+
+// create a loop to cycle through the time blocks
+for (let index = 9; index < 18; index++) {
+    var time = parseInt(document.getElementById("time" + index).id.replace("time", ""));
+    var block = document.getElementById("block" + index);
+    if (time < currentHour) {
+        block.classList.add("past")
+    } else if (time == currentHour) {
+        block.classList.add("present")
+    }
+    else {
+        block.classList.add("future")
+    }
 }
